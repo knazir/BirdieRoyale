20b45327c5573ed0aacdde60721af1e659f591bd
Kashif Nazir <kashifsnazir@gmail.com>
2022-12-10 16:05:08 -0800

    Initial project

---
+++ Source/BirdieRoyale/BirdieRoyaleGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BirdieRoyaleGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ABirdieRoyaleGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ABirdieRoyaleGameMode();
+};
+
+
+
