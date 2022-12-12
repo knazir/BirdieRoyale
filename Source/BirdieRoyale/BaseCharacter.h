226f3492ad21bb496e869df3efb937e40c517437
Kashif Nazir <kashifsnazir@gmail.com>
2022-12-12 02:05:28 -0800

    Move C++ properties into BP as well

--- Source/BirdieRoyale/BaseCharacter.h
+++ Source/BirdieRoyale/BaseCharacter.h
@@ -21,39 +21,6 @@ public:
 	virtual void Tick(float DeltaTime) override;
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-
 private:
-	// Blueprint Only
-	
-	UPROPERTY(BluePrintReadWrite, meta = (AllowPrivateAccess = "true"))
-	bool bIsSliding = false;
 
-	UPROPERTY(BluePrintReadWrite, meta = (AllowPrivateAccess = "true"))
-	float NextSlideBoostAvailableTs = 0.0f;
-
-	UPROPERTY(BluePrintReadWrite, meta = (AllowPrivateAccess = "true"))
-	float LastSlideStartedTs = 0.0f;
-
-	// Configurable
-	
-	UPROPERTY(EditAnywhere, BluePrintReadWrite, Category = "Movement", meta = (AllowPrivateAccess = "true"))
-	float SlideBoostCooldown = 2.0f;
-
-	UPROPERTY(EditAnywhere, BluePrintReadWrite, Category = "Movement", meta = (AllowPrivateAccess = "true"))
-	float SlideBoostStrength = 1200.0f;
-
-	UPROPERTY(EditAnywhere, BluePrintReadWrite, Category = "Movement", meta = (AllowPrivateAccess = "true"))
-	float VelocityInitialPushForceMultiplier = 150.0f;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement", meta = (AllowPrivateAccess = "true"))
-	float SlideExitDelay = 1.0f;
-	
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
-	float SlideTurnDampener = 0.25f;
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
-	float SlideFriction = 0.5f;
-	
-	UPROPERTY(EditAnywhere, BluePrintReadWrite, Category = "Movement", meta = (AllowPrivateAccess = "true"))
-	float PostSlideSpeed = 180.0;
 };
